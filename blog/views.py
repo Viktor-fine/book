c7894e9e0c30b9c468f358669f8ceb10c23d1a08
Viktor-fine <vik-mailll@mail.ru>
2025-07-17 23:05:06 +0300

    Добавлена система ограничения прав доступа

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 def index(request):
     return render(request, 'blog/index.html')
@@ -9,7 +10,7 @@ def index(request):
 @login_required # Этот код должен выполняться до кода def topics()
 def topics(request):
     """Выводит список всех тем из БД"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'blog/topics.html', context)
 
@@ -17,6 +18,12 @@ def topics(request):
 def topic(request, topic_id):
     """Выводит одну тему и все её записи"""
     topic = Topic.objects.get(id=topic_id)
+
+    # Проверко того, что тема принадлежит текущему пользователю
+    if topic.owner != request.user:
+        raise Http404
+
+
     entries = topic.entry_set.all()
     context = {'topic': topic, 'entries': entries}
     return render (request, 'blog/topic.html', context)
@@ -31,7 +38,9 @@ def new_topic(request):
         #Отправка данных методом POST и обработка.
         form = TopicForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_topic = form.save(commit=False)
+            new_topic.owner = request.user
+            new_topic.save()
             return redirect('blog:topics')
         #Вывести пустую форму
     context = {'form': form}
@@ -61,6 +70,9 @@ def edit_entry(request, entry_id):
     #Редактирует существующую запись
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
+    #Защитим эту страницу, чтобы никто не мог подобрать URL для получения доступа к чужим записям
+    if topic.owner != request.user:
+        raise Http404
 
     if request.method !='POST':
         #Исходный запрос. Форма заполняется данными текущей записи
